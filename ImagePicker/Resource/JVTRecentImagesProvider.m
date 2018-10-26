1a548e389fb43e7f7c5169bee376a0042a9db830
Thijs Bastiaens <thijs.bastiaens@gmail.com>
2018-10-26 17:06:40 +0200

    Add crash

--- ImagePicker/Resource/JVTRecentImagesProvider.m
+++ ImagePicker/Resource/JVTRecentImagesProvider.m
@@ -16,59 +16,61 @@ static NSInteger maxResults = 15;
 @implementation JVTRecentImagesProvider
 
 + (void)getRecentImagesWithSize:(CGSize) size return:(void (^)(NSArray<UIImage *> *images))callback {
-    [JVTCameraAccesebility getPhotoRollAccessibilityAndRequestIfNeeded:^(BOOL allowedToUseCamera) {
-        if (!allowedToUseCamera) {
-            dispatch_async(dispatch_get_main_queue(), ^{
-                callback(nil);
-            });
-        }
-        PHFetchResult *smartAlbums = [PHAssetCollection fetchAssetCollectionsWithType:PHAssetCollectionTypeSmartAlbum subtype:PHAssetCollectionSubtypeAlbumRegular options:nil];
-        
-        [smartAlbums enumerateObjectsUsingBlock:^(PHAssetCollection *collection, NSUInteger idx, BOOL *stop){
-            
-        }];
-        
-        PHFetchOptions *fetchOptions = [PHFetchOptions new];
-        fetchOptions.sortDescriptors = @[ [NSSortDescriptor sortDescriptorWithKey:@"creationDate" ascending:NO] ];
-        PHFetchResult *allPhotosResult = [PHAsset fetchAssetsWithMediaType:PHAssetMediaTypeImage options:fetchOptions];
-        
-        if (allPhotosResult.count == 0) {
-            dispatch_async(dispatch_get_main_queue(), ^{
-                callback(nil);
-            });
-            return;
-        }
-        
-        __block NSMutableArray *allImages = [NSMutableArray array];
-        
-        //   Get assets from the PHFetchResult object
-        [allPhotosResult enumerateObjectsUsingBlock:^(PHAsset *asset, NSUInteger idx, BOOL *stop) {
-            PHImageRequestOptions *options = [[PHImageRequestOptions alloc] init];
-            options.deliveryMode = PHImageRequestOptionsDeliveryModeHighQualityFormat; //I only want the highest possible quality
-            options.synchronous = YES;
-            options.networkAccessAllowed = YES;
-            options.progressHandler = ^(double progress, NSError *error, BOOL *stop, NSDictionary *info) {
-                NSLog(@"%f", progress); //follow progress + update progress bar
-            };
-            
-            __block PHImageRequestID reqId = [[PHImageManager defaultManager] requestImageForAsset:asset
-                                                                                        targetSize:size
-                                                                                       contentMode:PHImageContentModeAspectFill
-                                                                                           options:options
-                                                                                     resultHandler:^(UIImage *image, NSDictionary *info) {
-                                                                                         
-                                                                                         [allImages addObject:image];
-                                                                                         
-                                                                                         if (allImages.count == allPhotosResult.count || allImages.count >= maxResults) {
-                                                                                             *stop = YES;
-                                                                                             [[PHImageManager defaultManager] cancelImageRequest:reqId];
-                                                                                             dispatch_async(dispatch_get_main_queue(), ^{
-                                                                                                 callback(allImages);
-                                                                                             });
-                                                                                         }
-                                                                                     }];
-        }];
+  [JVTCameraAccesebility getPhotoRollAccessibilityAndRequestIfNeeded:^(BOOL allowedToUseCamera) {
+    if (!allowedToUseCamera) {
+      dispatch_async(dispatch_get_main_queue(), ^{
+        callback(nil);
+      });
+    }
+    PHFetchResult *smartAlbums = [PHAssetCollection fetchAssetCollectionsWithType:PHAssetCollectionTypeSmartAlbum subtype:PHAssetCollectionSubtypeAlbumRegular options:nil];
+    
+    [smartAlbums enumerateObjectsUsingBlock:^(PHAssetCollection *collection, NSUInteger idx, BOOL *stop){
+      
     }];
+    
+    PHFetchOptions *fetchOptions = [PHFetchOptions new];
+    fetchOptions.sortDescriptors = @[ [NSSortDescriptor sortDescriptorWithKey:@"creationDate" ascending:NO] ];
+    PHFetchResult *allPhotosResult = [PHAsset fetchAssetsWithMediaType:PHAssetMediaTypeImage options:fetchOptions];
+    
+    if (allPhotosResult.count == 0) {
+      dispatch_async(dispatch_get_main_queue(), ^{
+        callback(nil);
+      });
+      return;
+    }
+    
+    __block NSMutableArray *allImages = [NSMutableArray array];
+    
+    //   Get assets from the PHFetchResult object
+    [allPhotosResult enumerateObjectsUsingBlock:^(PHAsset *asset, NSUInteger idx, BOOL *stop) {
+      PHImageRequestOptions *options = [[PHImageRequestOptions alloc] init];
+      options.deliveryMode = PHImageRequestOptionsDeliveryModeHighQualityFormat; //I only want the highest possible quality
+      options.synchronous = YES;
+      options.networkAccessAllowed = YES;
+      options.progressHandler = ^(double progress, NSError *error, BOOL *stop, NSDictionary *info) {
+        NSLog(@"%f", progress); //follow progress + update progress bar
+      };
+      
+      __block PHImageRequestID reqId = [[PHImageManager defaultManager] requestImageForAsset:asset
+                                                                                  targetSize:size
+                                                                                 contentMode:PHImageContentModeAspectFill
+                                                                                     options:options
+                                                                               resultHandler:^(UIImage *image, NSDictionary *info) {
+                                                                                 
+                                                                                 if (image) {
+                                                                                   [allImages addObject:image];
+                                                                                 }
+                                                                                 
+                                                                                 if (allImages.count == allPhotosResult.count || allImages.count >= maxResults) {
+                                                                                   *stop = YES;
+                                                                                   [[PHImageManager defaultManager] cancelImageRequest:reqId];
+                                                                                   dispatch_async(dispatch_get_main_queue(), ^{
+                                                                                     callback(allImages);
+                                                                                   });
+                                                                                 }
+                                                                               }];
+    }];
+  }];
 }
 
 @end
